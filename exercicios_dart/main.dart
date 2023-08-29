6dadecd42b4391dcd8b86e58de56f7f7e3ea34eb
Andrea Oliveira <andreaferreiraoliveira@gmail.com>
2023-08-29 10:24:49 -0300

    projetos de teste

---
+++ exercicios_dart/main.dart
@@ -0,0 +1,10 @@
+main() {
+  int a = 3;
+  var c = 'Você é muito legal';
+  print(c is String);
+  var nomes = ['Ana', 'Bia', 'Carlos'];
+  nomes.add('Daniel');
+  print(nomes.length);
+  print(nomes.elementAt(0));
+  print(nomes[3]);
+}
